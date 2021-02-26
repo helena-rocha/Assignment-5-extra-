eb9ba6bbbbe71f763630c22caa44cd230970f351
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-26 21:43:17 +0000

    New code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <math.h>
+
+#define _USE_MATH_DEFINES
 
 int main() {
-  std::cout << "Hello World!\n";
+  float diameter, circumference, area;
+  std::cout << "Program for calculating the area and circumference of a circle based on the input of the circle's diameter!\n";
+  std::cout << "Please input the diameter:\n";
+  std::cin >> diameter;
+  circumference = M_PI * diameter;
+  area = M_PI * pow (diameter, 2);
+  std::cout << "The circumference is "<< circumference << "\n";
+  std::cout << "The area is "<< area << "\n";
+  std::cout << "Thank you for using this code!!!\n";  
 }
